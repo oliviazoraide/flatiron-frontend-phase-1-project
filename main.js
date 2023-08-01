2f042a4b5b8f1a91bd749036959caf8f6c47b514
Olivia Desgrossillier <oliviadesgrossillier@gmail.com>
2023-08-01 03:19:53 -0700

    create variable for author name

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ console.log("connected")
 //Variables for code:
 let button = document.querySelector('#new-book');
 let bookTitle = document.querySelector('.book-title');
+let authorName = document.querySelector('.author-name');
 document.addEventListener()
